077dbff5c04af1aa7aaa650d4316df8eb215e396
justyna0bartoszewska <32938098+justyna0bartoszewska@users.noreply.github.com>
2018-06-15 15:44:23 +0200

    Add files via upload

---
+++ brief-www.js
@@ -0,0 +1,65 @@
+$("#brief-www").validator().on("submit", function (event) {
+    if (event.isDefaultPrevented()) {
+        // handle the invalid form...
+        formError();
+        submitMSG(false, "Wystąpił błąd! Sprawdź czy wypełniono poprawnie formularz.");                            
+    } else {
+        // everything looks good!
+        event.preventDefault();
+        submitForm();
+    }
+});
+
+function submitForm(){
+    // Initiate Variables With Form Content
+
+	var inputName = $("#inputName") .val();
+	var inputDomena = $("#inputDomena") .val();
+	var inputTematyka = $("#inputTematyka") .val();
+	var rodzajStrony = $("#rodzajStrony") .val();
+	var skladProjektu = $("#skladProjektu") .val();
+	var TytulyZakladek = $("#TytulyZakladek") .val();
+	var adresurl = $("#adresurl") .val();	
+	var teksty= $("#teksty") .val();	
+	var szablon = $("#szablon") .val();	
+	var srodowisko = $("#srodowisko") .val();	
+	var Inspiracje = $("#Inspiracje") .val();	
+	var Budzet = $("#Budzet") .val();	
+	var DodatkowyOpis = $("#DodatkowyOpis") .val();	
+	var zalacznik = $("#zalacznik") .val();	
+	var zgoda = $("#zgoda") .val();	
+ 
+	$.ajax({
+      type: "POST",
+      url: "brief-www.php",
+      data: $('form#brief-www').serialize(),
+      success : function(text){
+          if (text == "success"){
+              formSuccess();
+          } else {
+              formError();
+              submitMSG(false,text);
+          }
+      }
+  });
+}
+
+function formSuccess(){
+    $("#brief-www")[0].reset();
+    submitMSG(true, "Zapytanie zostało pomyślnie wysłane! Skontaktujemy się z Tobą.")
+}
+
+function formError(){
+    $("#brief-www").removeClass().addClass('shake animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+        $(this).removeClass();
+    });
+}
+
+function submitMSG(valid, msg){
+    if(valid){
+        var msgClasses = "h3 text-center tada animated text-success";
+    } else {
+        var msgClasses = "h3 text-center text-danger";
+    }
+    $("#msgSubmit").removeClass().addClass(msgClasses).text(msg);
+}
---
+++ brief-www.php
@@ -0,0 +1,105 @@
+﻿<?php
+
+$inputName = $_POST['inputName'];
+$inputEmail = $_POST['inputEmail'];
+$inputDomena = $_POST["inputDomena"];
+$inputTematyka = $_POST["inputTematyka"];
+$rodzajStrony = $_POST["rodzajStrony"];
+$skladProjektu = $_POST["skladProjektu"];
+$TytulyZakladek = $_POST["TytulyZakladek"];
+$adresurl = $_POST["adresurl"];
+$teksty = $_POST["teksty"];
+$szablon = $_POST["szablon"];
+$srodowisko = $_POST["srodowisko"];
+$system = $_POST["system"];
+$Inspiracje = $_POST["Inspiracje"];
+$Budzet = $_POST["Budzet"];
+$DodatkowyOpis = $_POST["DodatkowyOpis"];
+
+
+$EmailTo = "biuro@lowwwe.pl";
+$Subject = "Serwis www";
+ 
+// prepare email body text
+// prepare email body text
+$Body = "";
+$Body .= "Pełne dane kontaktowe zamawiającego: ";
+$Body .= $inputName;
+$Body .= "\n";
+ 
+$Body .= "Adres email zamawiającego: ";
+$Body .= $inputEmail;
+$Body .= "\n";
+ 
+$Body .= "Nazwa domeny: ";
+$Body .= $inputDomena;
+$Body .= "\n";
+ 
+$Body .= "Tematyka strony/branża: ";
+$Body .= $inputTematyka;
+$Body .= "\n";
+ 
+$Body .= "Rodzaj strony: ";
+$Body .= $rodzajStrony;
+$Body .= "\n";				
+
+$Body .= "Skład projektu: ";
+$Body .= $skladProjektu;
+$Body .= "\n";	
+
+$Body .= "Tytuły zakładek: ";
+$Body .= $TytulyZakladek;
+$Body .= "\n";
+
+$Body .= "Link do materiałów: ";
+$Body .= $adresurl;
+$Body .= "\n";
+
+$Body .= "Przygotowanie tekstów na strone: ";
+$Body .= $teksty;
+$Body .= "\n";
+
+$Body .= "Szablon landing page: ";
+$Body .= $szablon;
+$Body .= "\n";
+
+$Body .= "Serwer i środowisko: ";
+$Body .= $srodowisko;
+$Body .= "\n";
+
+$Body .= "System budowania strony: ";
+$Body .= $system;
+$Body .= "\n";
+
+$Body .= "Inspiracje: ";
+$Body .= $Inspiracje;
+$Body .= "\n";
+
+$Body .= "Budżet: ";
+$Body .= $Budzet;
+$Body .= "\n";
+
+$Body .= "Dodatkowy Opis: ";
+$Body .= $DodatkowyOpis;
+$Body .= "\n";
+
+$Body .= "Załącznik: ";
+$Body .= $zalacznik;
+$Body .= "\n";
+
+
+$Body .= "Wyrażam zgodę na przetwarzanie przez www.lowwwe.pl moich danych osobowych podanych w formularzu w celu udzielenia odpowiedzi na zadane pytanie, zgodnie z przepisami Rozporządzenia Parlamentu Europejskiego i Rady (UE) 2016/679 z 27 kwietnia 2016 r. i przepisami krajowymi dotyczącymi ochrony danych osobowych, w tym przesyłania ich drogą elektroniczną. ";
+$Body .= $zgoda;
+$Body .= "\n";
+
+// send email
+$success = mail($EmailTo, $Subject, $Body,  "From:".$email);
+
+// redirect to success page
+if ($success){
+   echo "success";
+}else{
+    echo "invalid";
+}
+ 
+?>
